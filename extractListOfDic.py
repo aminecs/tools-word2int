f26b8e6dc06ca86df1f22718434393e964d69a2d
aminecs <m.amine.belabbes@gmail.com>
2017-12-05 15:15:11 +0000

    Change name of argument

--- extractListOfDic.py
+++ extractListOfDic.py
@@ -4,18 +4,18 @@ def readFileMap(fileArg):
     with open(fileArg) as fileInput:
         return fileInput.read()
 
-def returnListofDico(fileArg):
-    map = readFileMap(fileArg)
+def listofDico(path):
+    map = readFileMap(path)
     myJson = json.loads(map)
     return myJson ['nodes']
 
-def writeTo(fileArg):
-    list = returnListofDico(fileArg)
+def writeTo(path):
+    list = listofDico(path)
     with open('listofDico', 'xt') as fileOutput:
         for dico in list:
             fileOutput.write("%s\n" % dico)
 
-def main(fileArg):
-    writeTo(fileArg)
+def main(path):
+    writeTo(path)
 
 main('map.json')
